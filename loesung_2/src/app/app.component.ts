6304be205e77b99734ffddc634e6dcc99be10ee2
Daniel Reinert <daniel.reinert@viadee.de>
2024-09-23 16:52:41 +0200

    loesung 2: update to 18

--- loesung_2/src/app/app.component.ts
+++ loesung_2/src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
 import { LowerCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [FormsModule, LowerCasePipe]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule, LowerCasePipe],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Fruit Shop Capstone';
-
+  title = 'Welcome';
+  zeigtWelcome = true;
   public fruit = '';
+
+
+  public changeTitle() {
+    this.title = this.zeigtWelcome ?  'Hello':'Welcome';
+    this.zeigtWelcome = !this.zeigtWelcome;
+  }
 
   public resetFruit(): void {
     this.fruit = '';
