4f4992584998c1a43c1f875a6b481ac2adb785c2
Max Strohbuecker <max.strohbuecker@viadee.de>
2024-09-24 17:47:27 +0200

    feat(loesung-4): implement if and for using control flow templating

--- loesung_4/src/app/app.component.spec.ts
+++ loesung_4/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'loesung_2' title`, () => {
+  it(`should have the 'loesung_4' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('loesung_2');
+    expect(app.title).toEqual('loesung_4');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, loesung_2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, loesung_4');
   });
 });
